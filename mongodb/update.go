dad4fdc3b583f65ddbfec3db46dcb626432e0890
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-10-31 03:31:29 +0530

    UpdateAllNames

--- mongodb/update.go
+++ mongodb/update.go
@@ -19,3 +19,17 @@ func (db *DB) SwapOneAvailability(name string, price float64, available bool) er
 	return result.Err()
 
 }
+
+func (db *DB) UpdateAllNames(previousName string, newName string) (int64, error) {
+	collection := db.client.Database(dbName).Collection(collectionName)
+
+	// Define the filter to match documents by "name"
+	filter := bson.D{{"name", previousName}}
+
+	// Define the update operation to set "availability" to a new value
+	update := bson.D{{"$set", bson.D{{"name", newName}}}}
+
+	result, err := collection.UpdateMany(db.ctx, filter, update)
+
+	return result.ModifiedCount, err
+}
