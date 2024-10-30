3cfa1ca894d4ff6c8b18ba15fa46d27cc64580ad
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-10-31 02:54:45 +0530

    get by name added in mongodb

--- mongodb/get.go
+++ mongodb/get.go
@@ -33,3 +33,35 @@ func (db *DB) GetAll() ([]structs.Product, error) {
 	return dataCollection, nil
 
 }
+
+func (db *DB) GetByName(name string, availability bool) ([]structs.Product, error) {
+	collection := db.client.Database(dbName).Collection(collectionName)
+
+	// 	pass mongodb options to find function
+	findOption := options.Find()
+
+	filter := bson.D{
+		{"name", name},
+		{"available", availability},
+	}
+
+	var dataCollection []structs.Product
+	rows, err := collection.Find(db.ctx, filter, findOption)
+	if err != nil {
+		return dataCollection, err
+	}
+
+	defer rows.Close(db.ctx)
+
+	var data structs.Product
+
+	for rows.Next(db.ctx) {
+		err = rows.Decode(&data)
+		if err != nil {
+			return dataCollection, err
+		}
+		dataCollection = append(dataCollection, data)
+	}
+
+	return dataCollection, nil
+}
